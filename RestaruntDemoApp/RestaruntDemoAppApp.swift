e318843ae54b755ab928658a0c429ad57c6d50bb
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-13 11:37:46 +0530

    Initial Commit

---
+++ RestaruntDemoApp/RestaruntDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaruntDemoAppApp.swift
+//  RestaruntDemoApp
+//
+//  Created by Knoxpo MacBook Pro on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaruntDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
